cc4405293dd940f6ede45cac16936e9ae2facbf5
Da Huo <huodamelbourne@gmail.com>
2020-04-28 20:38:27 +1000

    feat: lesson6

--- src/containers/TodoList/__tests__/unit/TodoList.js
+++ src/containers/TodoList/__tests__/unit/TodoList.js
@@ -10,7 +10,7 @@ it('TodoList 初始化列表为空', () => {
 it('TodoList 应该给 Header 传递一个增加undoList内容的方法', () => {
     const wrapper = shallow( <TodoList /> )
     const Header = wrapper.find('Header')
-    expect(Header.prop('addUndoItem')).toBe(wrapper.instance().addUndoItem)
+    expect(Header.prop('addUndoItem')).toBeTruthy()
 })
 /**
  * 1. TodoList中有一个自元素，叫<Header />，所以用find可以找到这个元素
@@ -18,18 +18,36 @@ it('TodoList 应该给 Header 传递一个增加undoList内容的方法', () => 
  *    也就是说，传递接收到的参数中的函数 = 父组件实例中的函数
  */
 
-it('当Header 回车的时候，undoList应该增加内容', () => {
+it('addUndoItem被执行的时候，undoList应该增加内容', () => {
     const wrapper = shallow( <TodoList /> )
-    const Header = wrapper.find('Header')
-    const addFunc = Header.prop('addUndoItem')
     const inputValue = '学习 React'
-    addFunc(inputValue)
+    wrapper.instance().addUndoItem(inputValue)
     expect(wrapper.state('undoList').length).toBe(1)
     expect(wrapper.state('undoList')[0]).toBe(inputValue)
-    addFunc('学习 React2')
+    wrapper.instance().addUndoItem('学习 React2')
     expect(wrapper.state('undoList').length).toBe(2)
 })
 /**
  * 应该尽可能将header与todoList解耦，所以不要尝试模拟header的回车方法，而是直接调用回车之后会触发的方法
  * 
+ */
+
+it('TodoList 应该给 Header 传递一个增加undoList内容的方法', () => {
+    const wrapper = shallow( <TodoList /> )
+    const UndoList = wrapper.find('UndoList')
+    expect(UndoList.prop('list')).toBeTruthy()
+    expect(UndoList.prop('deleteItem')).toBeTruthy()
+})
+
+it('当delete方法被执行的时候，undoList应该删除内容', () => {
+    const wrapper = shallow( <TodoList /> )
+    wrapper.setState({
+        undoList: ['学习 Jest', 'dell', 'lee']
+    })
+    wrapper.instance().deleteItem(1)
+    expect(wrapper.state('undoList')).toEqual(['学习 Jest', 'lee'])
+})
+
+/**
+ * 14min后面的refactor没加
  */
